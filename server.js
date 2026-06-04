de8f55550bc7a93c27a057ea538ce271b0376037
jiangxiongming <1204808087@qq.com>
2026-06-04 18:47:40 +0800

    fix: use PORT env var for Zeabur

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const fs = require('fs');
 const path = require('path');
 const url = require('url');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const OPENMAIC_URL = "https://zfx2026.zeabur.app";
 
 // MIME types
